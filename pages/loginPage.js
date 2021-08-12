e69615bda3de24d050a5d1a3f5498f9ff66a5246
Ben Brumm <ben@databasestar.com>
2021-08-13 09:53:17 +1000

    First set of tests

---
+++ pages/loginPage.js
@@ -0,0 +1,35 @@
+//jshint esversion:8
+class LoginPage {
+
+    constructor(page) {
+        this.page = page;
+    }
+
+    async getTitle() {
+        return this.page.title();
+    }
+
+    async usernameFieldExists() {
+      //return await this.page.waitForSelector('input[id=user-name]') !== null;
+      return await this.checkIfElementExists('user-name');
+    }
+
+    async passwordFieldExists() {
+      //return await this.page.waitForSelector('input[id=password]') !== null;
+      return await this.checkIfElementExists('password');
+    }
+
+    async checkIfElementExists(elementID) {
+      return await this.page.waitForSelector('input[id='+ elementID +']') !== null;
+    }
+
+    /*
+    async searchFor(word) {
+        await this.page.type('input[id=search_form_input_homepage]', word);
+        await this.page.click('input[type="submit"]');
+    }
+    */
+
+}
+
+module.exports = LoginPage;
---
+++ test/loginTest.js
@@ -0,0 +1,59 @@
+//jshint esversion:8
+const puppeteer = require('puppeteer');
+const expect = require('chai').expect;
+const LoginPage = require('../pages/LoginPage');
+
+
+describe('Login Page for Standard User', () => {
+  let browser;
+  let page;
+
+  beforeEach(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('https://www.saucedemo.com/');
+  });
+
+  afterEach(async () => {
+    await browser.close();
+  });
+
+  /*
+  it('should have the correct title async', async () => {
+    this.timeout(5000);
+    const loginPage = new LoginPage(page);
+    let actualTitle = await loginPage.getTitle();
+    expect(actualTitle).to.eql('Swag Labs');
+  });
+  */
+
+  it('should have the correct title (with timeout)', function(done) {
+    this.timeout(5000);
+    (async () => {
+      const loginPage = new LoginPage(page);
+      let actualTitle = await loginPage.getTitle();
+      expect(actualTitle).to.eql('Swag Labs');
+    })().then(done);
+  });
+
+  it('incorrect title test', function(done) {
+    this.timeout(5000);
+    (async () => {
+      const loginPage = new LoginPage(page);
+      let actualTitle = await loginPage.getTitle();
+      expect(actualTitle).to.not.eql('Wrong Value');
+    })().then(done);
+  });
+  
+
+  it('username field exists', async () => {
+    const loginPage = new LoginPage(page);
+    expect(await loginPage.usernameFieldExists()).to.eql(true);
+  });
+
+  it('password field exists', async () => {
+    const loginPage = new LoginPage(page);
+    expect(await loginPage.passwordFieldExists()).to.eql(true);
+  });
+
+});
